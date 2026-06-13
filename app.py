db60dd0db0e3bfc2e18669f3d2593347da8f21d9
Hafsa Shabbeer <hafsashabbeer@gmail.com>
2026-06-13 12:45:34 -0400

    Initial commit - Hafsa Shabbeer

---
+++ app.py
@@ -0,0 +1,8 @@
+# app.py - Created by Hafsa Shabbeer
+from unicodedata import name
+
+
+def greet(name):
+    return f"Hello, {name}! Welcome to AIDI 2004."
+if __name__ == "__main__":
+    print(greet("Hafsa Shabbeer"))
